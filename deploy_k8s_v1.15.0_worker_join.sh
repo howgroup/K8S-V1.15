b08794b20d25bd54e6563545b67695a09169f292
hcdaycn <hcday@qq.com>
2019-09-11 19:45:24 +0800

    将master文件和worker文件流程单独拆分

--- deploy_k8s_v1.15.0_worker_join.sh
+++ deploy_k8s_v1.15.0_worker_join.sh
@@ -11,7 +11,7 @@ if [[ "$(whoami)" != "root" ]]; then  #check with user, it must be root
     exit 1
 fi
 
-log="./setup_master.log"  #log file path,操作日志存放路径
+log="./setup_worker_join.log"  #log file path,操作日志存放路径
 fsize=2000000
 exec 2>>$log  #save all logs to setup log file,如果执行过程中有错误信息均输出到日志文件中
 
@@ -19,13 +19,16 @@ echo -e "\033[31m 这个是Kubernetes集群一键部署脚本,当前部署版本
 sleep 5
 
 
-#初始化K8S
-init_k8s(){
+#初始化K8S,join到集群中
+init_k8s_worker_join(){
     set -e
     rm -rf /root/.kube
     kubeadm reset -f
 
-    kubeadm init --kubernetes-version=$k8s_version --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$masterip
+    #kubeadm init --kubernetes-version=$k8s_version --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$masterip
+
+    kubeadm join $masterip:6443 --token $token \
+    --discovery-token-ca-cert-hash sha256:$sha_text \
 
     mkdir -p /root/.kube
     cp /etc/kubernetes/admin.conf /root/.kube/config
@@ -38,6 +41,7 @@ init_k8s(){
 
 #主程序入口
 main(){
-  init_k8s
+  init_k8s_worker_join
 }
-main > ./setup_master.log 2>&1
+
+main > ./setup_worker_join.log 2>&1

7e3a5e737dae4582193a7f946fac2ef307bb38e3
hcdaycn <hcday@qq.com>
2019-09-11 19:12:22 +0800

    增加join的sh文件

--- clean_k8s.sh
+++ clean_k8s.sh
@@ -4,13 +4,15 @@
     systemctl stop kubelet
     docker rm -f -v $(docker ps  -a -q)
 
+    yum remove kubeadm
+    yum remove kubelet
+    yum remove kubectl
+
     rm -rf /etc/kubernetes
     rm -rf  /var/lib/etcd
     rm -rf   /var/lib/kubelet
     rm -rf  $HOME/.kube/config
     iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
-    yum reinstall -y kubelet
     systemctl daemon-reload
     systemctl restart docker
-    systemctl enable kubelet
